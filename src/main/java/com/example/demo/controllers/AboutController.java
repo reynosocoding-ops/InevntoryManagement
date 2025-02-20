a273455fdd7dd092352ddba924148342574a46c1
Jesus Reynoso <reynosoprof@gmail.com>
2025-02-20 09:40:14 -0800

    finished section F

--- src/main/java/com/example/demo/controllers/AboutController.java
+++ src/main/java/com/example/demo/controllers/AboutController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AboutController {
-    @GetMapping("About.html")
+    @GetMapping("/About")
     public String returnAboutPage() {
-        return "About.html";
+        return "About";
     }
 }
 
